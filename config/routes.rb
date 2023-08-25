ccc5c3d7fd2672164230f923317e6135ee325a9c
sindpet <137641233+sindpet@users.noreply.github.com>
2023-08-25 22:02:41 +0200

    Add static page controller, add simple form and display of user photos

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get '/', to: "static_pages#index"
+  post '/', to: "static_pages#index"
   # Defines the root path route ("/")
   # root "articles#index"
 end
