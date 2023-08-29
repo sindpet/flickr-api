8e300e36180a631f01ee77694b95cb0ae0d11fcc
sindpet <137641233+sindpet@users.noreply.github.com>
2023-08-29 17:07:31 +0200

    Remove puts statements

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,13 +5,10 @@ class StaticPagesController < ApplicationController
     if params[:flickr_user_id]
       flickr = Flickr.new
       photos = flickr.people.getPhotos(user_id: params[:flickr_user_id])
-      puts photos.to_json
 
       @photo_urls = photos.map do |photo|
         "https://live.staticflickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + "_q.jpg"
       end
-
-      p @photo_urls
     end
   end
 end
